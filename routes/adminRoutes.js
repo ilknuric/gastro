31a32131cb294d91773213c297e7a12f297c029f
ilknuric <ilknurcengiz7@gmail.com>
2024-11-27 14:01:33 +0300

    Switch to bcryptjs for better compatibility

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
 const path = require('path');
 const multer = require('multer');
 const ejs = require('ejs');
